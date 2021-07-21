c1fdd29f9440d63ff7a07329d1550b2f3e38875f
silvergl <43707655+silvergl@users.noreply.github.com>
2021-07-22 00:49:10 +0200

    metatrace

--- src/Record.py
+++ src/Record.py
@@ -47,3 +47,40 @@ class OperationExecutionRecord:
         self.hostname = self.__NO_HOSTNAME__ if hostname is None else hostname
         self.eoi = eoi
         self.ess = ess
+        
+        def serialize(self,serializer):
+            serializer.put(self.operation_signature)
+            serializer.put(self.session_id)
+            serializer.put(self.tin)
+            serializer.put(self.tout)
+            serializer.put(self.hostname)
+            serializer.put(self.eoi)
+            serializer.put(self.ess)
+class TraceMetadata:
+    __NO_PARENT_TRACEID__ = -1
+    __NO_PARENT_ORDER_INDEX__ = -1
+    __NO_SESSION_ID__ = "<no-session-id>"
+    __NO_HOSTNAME__ = "<default-host>"
+    def __init__(self,trace_id, thread_id,
+                 session_id, hostname,
+                 parent_trace_id,
+                 parent_order_id):
+        self.trace_id=trace_id
+        self.thread_id=thread_id
+        self.session_id = (self.__NO_SESSION_ID__
+                           if session_id is None
+                           else session_id)
+        self.hostname = (self.__NO_HOASTNAME__
+                           if hostname is None
+                           else hostname)
+        self.parent_trace_id=parent_trace_id
+        self.parent_order_id=parent_order_id
+        pass
+    def serialize(self,serializer):
+        serializer.put(self.trace_id)
+        serializer.put(self.thread_id)
+        serializer.put(self.session_id)
+        serializer.put(self.hostname)
+        serializer.put(self.parent_trace_id)
+        serializer.put(self.parent_order_id)
+        
