dce7a060bbf2c6152b9ceb3ca3e70bd3c9541c30
silvergl <simonovserafim@gmail.com>
2022-02-18 17:09:54 +0100

    build.sh

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PATH_KIEKER='/home/serafim/Desktop/kieker-lang-pack-python'
+PATH_SPYDER='/home/serafim/Desktop/spyder'
+PATH_COLLECTOR='/home/serafim/Desktop/collector-1.15-SNAPSHOT/bin/collector.sh'
+
+
+cd $PATH_KIEKER
+python3 -m build
+pip install dist/kieker-monitoring-for-python-0.0.1.tar.gz
+
+
+if [[ $1 == -nc ]]
+then 
+
+     gnome-terminal -- bash -c "nc -l 65432"
+
+     cd $PATH_SPYDER
+     python3 bootstrap.py 
+    
+elif [[ $1 == -kieker ]]
+then
+     gnome-terminal -- bash -c "${PATH_COLLECTOR} -c config.txt"
+     echo"wait 45 seconds to let collector initialize properly"
+     sleep 45s
+     cd $PATH_SPYDER
+     python3 bootstrap
+
+else
+	echo "Wrong command. Supported commands: [-nc],[-kieker]"
+	echo "[-nc] debug using netcat"
+	echo "[-kieker] debug using collector"
+fi    
+
