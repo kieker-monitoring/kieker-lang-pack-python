ee0d3e123495fb476a24255511a1b1a7c523336f
Shinhyung Yang <shinhyung.yang@email.uni-kiel.de>
2026-06-01 12:03:44 +0200

    Change version strings

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kieker-monitoring-for-python",
-    version="0.0.1",
+    version="0.0.2",
     author="Serafim Simonov",
     author_email="stu126367@mail.uni-kiel.de",
     description="Implementation of kieker-monitoring for pyhton",
